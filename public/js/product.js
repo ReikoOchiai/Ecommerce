8e65109d95bba915a88260db16c65b4c58a3d032
ReikoOchiai <lissage.23@gmail.com>
2022-04-06 22:02:07 +1000

    toggle size buttons

--- public/js/product.js
+++ public/js/product.js
@@ -14,3 +14,14 @@ productImages.forEach((item, i) => {
 });
 
 // toggle size buttons
+
+const sizeBtns = document.querySelectorAll(".size-radio-btn");
+let checkedBtn = 0;
+
+sizeBtns.forEach((item, i) => {
+  item.addEventListener("click", () => {
+    sizeBtns[checkedBtn].classList.remove("check");
+    item.classList.add("check");
+    checkedBtn = i;
+  });
+});
